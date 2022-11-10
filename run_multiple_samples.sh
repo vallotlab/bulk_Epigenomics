da3637f70b4496c8171ff46bbae03f20a9d45770
pprompsy <pacome.pr@gmail.com>
2022-11-10 13:37:23 +0100

    launch qsub

--- run_multiple_samples.sh
+++ run_multiple_samples.sh
@@ -25,4 +25,4 @@ rm -rf $output_dir/logs/*.log
 python3 Scripts/sample2json.py -i $sample_sheet -o $output_dir/ -c $output_dir/species_design_configs.tsv 
 
 # Launch the pipeline using Singularity Image
-echo 'singularity  exec --bind ${bind_directory}:${bind_directory} --bind ${output_dir}:/mnt/ $image /bin/bash -c \"/mnt/run_bulk_Epigenomics.sh ${params}\"'
+echo "singularity  exec --bind ${bind_directory}:${bind_directory} --bind ${output_dir}:/mnt/ $image /bin/bash -c \"/mnt/run_bulk_Epigenomics.sh ${params}\"""
